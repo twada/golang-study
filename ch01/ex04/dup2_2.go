cd775fdfc5c45311401c003154e6a73e467aba05
Takuto Wada <takuto.wada@gmail.com>
2018-02-02 02:50:00 +0900

    range の戻り値などの記憶が誤っていた

--- ch01/ex04/dup2_2.go
+++ ch01/ex04/dup2_2.go
@@ -7,25 +7,32 @@ import (
 )
 
 func main() {
-	args := os.Args[1:]
 	counts := make(map[string]int)
-	if len(args) == 0 {
+	files := os.Args[1:]
+	if len(files) == 0 {
 		countLines(os.Stdin, counts)
 	} else {
-		for filename := range args {
-			f, err := os.Open(filename)
+		for _, arg := range files {
+			f, err := os.Open(arg)
 			if err != nil {
 				fmt.Fprintf(os.Stderr, "dup2_2: %v\n", err)
 				continue
 			}
 			countLines(f, counts)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
 }
 
-func conutLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File, counts map[string]int) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
 	}
+	// 注意: input.Err() からのエラーの可能性を無視している
 }
