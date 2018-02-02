3d297b5ac68899dad25e5b1ff69cd0c62afa7cd6
Takuto Wada <takuto.wada@gmail.com>
2018-02-02 17:19:48 +0900

    Occurrence という型を導入して解いてみた

--- ch01/ex04/dup2_2.go
+++ ch01/ex04/dup2_2.go
@@ -7,26 +7,30 @@ import (
 	"strings"
 )
 
+type Occurrence struct {
+	count int
+	files []string
+}
+
 func main() {
-	counts := make(map[string][]string)
+	occurrences := make(map[string]*Occurrence)
 	files := os.Args[1:]
 	if len(files) == 0 {
 		fmt.Fprintf(os.Stderr, "USAGE: %s [file1, file2, ...]\n", os.Args[0])
 		os.Exit(1)
 		return
 	}
-	for _, arg := range files {
-		countLinesInFile(arg, counts)
+	for _, file := range files {
+		countLinesInFile(file, occurrences)
 	}
-	for line, dups := range counts {
-		n := len(dups)
-		if n > 1 {
-			fmt.Printf("%d\t%s\t%v\n", n, line, strings.Join(dups, ","))
+	for line, occ := range occurrences {
+		if occ.count > 1 {
+			fmt.Printf("%d\t%s\t%v\n", occ.count, line, strings.Join(occ.files, ","))
 		}
 	}
 }
 
-func countLinesInFile(filename string, counts map[string][]string) {
+func countLinesInFile(filename string, occurrences map[string]*Occurrence) {
 	f, err := os.Open(filename)
 	defer f.Close()
 	if err != nil {
@@ -36,13 +40,18 @@ func countLinesInFile(filename string, counts map[string][]string) {
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		text := input.Text()
-		if (indexOf(counts[text], filename) == -1) {
-			counts[text] = append(counts[text], filename)
+		if _, ok := occurrences[text]; !ok {
+			occurrences[text] = new(Occurrence)
+		}
+		occ := occurrences[text]
+		occ.count++
+		if (indexOf(filename, occ.files) == -1) {
+			occ.files = append(occ.files, filename)
 		}
 	}
 }
 
-func indexOf(haystack []string, needle string) int {
+func indexOf(needle string, haystack []string) int {
 	for i, entry := range haystack {
 		if (entry == needle) {
 			return i
