3fa8b6172d0db96b21b2cd58542a46b484ce7afe
Takuto Wada <takuto.wada@gmail.com>
2018-02-02 16:38:56 +0900

    ファイルの数を数えるアプローチに変える途中

--- ch01/ex04/dup2_2.go
+++ ch01/ex04/dup2_2.go
@@ -4,35 +4,49 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
-	counts := make(map[string]int)
+	counts := make(map[string][]string)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
-	} else {
-		for _, arg := range files {
-			f, err := os.Open(arg)
-			if err != nil {
-				fmt.Fprintf(os.Stderr, "dup2_2: %v\n", err)
-				continue
-			}
-			countLines(f, counts)
-			f.Close()
-		}
+		fmt.Fprintf(os.Stderr, "USAGE: %s [file1, file2, ...]\n", os.Args[0])
+		os.Exit(1)
+		return
 	}
-	for line, n := range counts {
+	for _, arg := range files {
+		countLinesInFile(arg, counts)
+	}
+	for line, dups := range counts {
+		n := len(dups)
 		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
+			fmt.Printf("%d\t%s\t%v\n", n, line, strings.Join(dups, ","))
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLinesInFile(filename string, counts map[string][]string) {
+	f, err := os.Open(filename)
+	defer f.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "dup2_2: %v\n", err)
+		return
+	}
 	input := bufio.NewScanner(f)
 	for input.Scan() {
-		counts[input.Text()]++
+		text := input.Text()
+		if (indexOf(counts[text], filename) == -1) {
+			counts[text] = append(counts[text], filename)
+		}
 	}
-	// 注意: input.Err() からのエラーの可能性を無視している
+}
+
+func indexOf(haystack []string, needle string) int {
+	for i, entry := range haystack {
+		if (entry == needle) {
+			return i
+		}
+	}
+	return -1
 }
