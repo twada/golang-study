36b6f90e2df9b1354d4ffee136cc44407aeefe44
Takuto Wada <takuto.wada@gmail.com>
2018-02-04 14:54:34 +0900

    練習問題 1.8 strings.HasPrefix を使う

--- ch01/ex08/fetch.go
+++ ch01/ex08/fetch.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 	"io/ioutil"
 	"net/http"
 	"os"
@@ -9,6 +10,9 @@ import (
 
 func main() {
 	for _, url := range os.Args[1:] {
+		if !strings.HasPrefix(url, "http://") {
+			url = "http://" + url
+		}
 		resp, err := http.Get(url)
 		if err != nil {
 			fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
