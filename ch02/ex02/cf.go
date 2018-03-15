03b30ec3fe122e7b28ebb10c47d2aca8d540ed28
Takuto Wada <takuto.wada@gmail.com>
2018-03-16 06:50:06 +0900

    練習問題2.2 引数が指定されなかったら標準入力から読み込む

--- ch02/ex02/cf.go
+++ ch02/ex02/cf.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 	"strconv"
@@ -9,15 +10,27 @@ import (
 )
 
 func main() {
-	for _, arg := range os.Args[1:] {
-		t, err := strconv.ParseFloat(arg, 64)
-		if err != nil {
-			fmt.Fprintf(os.Stderr, "cf: %v\n", err)
-			os.Exit(1)
+	values := os.Args[1:]
+	if len(values) == 0 {
+		input := bufio.NewScanner(os.Stdin)
+		for input.Scan() {
+			conv(input.Text())
 		}
-		f := tempconv.Fahrenheit(t)
-		c := tempconv.Celsius(t)
-		fmt.Printf("%s = %s, %s = %s\n",
-			f, tempconv.FToC(f), c, tempconv.CToF(c))
+	} else {
+		for _, arg := range values {
+			conv(arg)
+		}
 	}
+}
+
+func conv(v string) {
+	t, err := strconv.ParseFloat(v, 64)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "cf: %v\n", err)
+		os.Exit(1)
+	}
+	f := tempconv.Fahrenheit(t)
+	c := tempconv.Celsius(t)
+	fmt.Printf("%s = %s, %s = %s\n",
+		f, tempconv.FToC(f), c, tempconv.CToF(c))
 }
