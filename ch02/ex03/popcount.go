8ab6858887444f64d175a6454f8cd0681c24089d
Takuto Wada <takuto.wada@gmail.com>
2018-03-16 08:51:05 +0900

    練習問題2.3 ループを使うように PopCount を修正

--- ch02/ex03/popcount.go
+++ ch02/ex03/popcount.go
@@ -9,12 +9,10 @@ func init() {
 }
 
 func PopCount(x uint64) int {
-	return int(pc[byte(x>>(0*8))] +
-		pc[byte(x>>(1*8))] +
-		pc[byte(x>>(2*8))] +
-		pc[byte(x>>(3*8))] +
-		pc[byte(x>>(4*8))] +
-		pc[byte(x>>(5*8))] +
-		pc[byte(x>>(6*8))] +
-		pc[byte(x>>(7*8))])
+	var acc int
+	var i uint
+	for i = 0; i < 8; i++ {
+		acc += int(pc[byte(x>>(i*8))])
+	}
+	return acc
 }
