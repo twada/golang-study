232c293ce8b0feabf278503e08448bb85a7a6bd3
Takuto Wada <takuto.wada@gmail.com>
2018-03-16 10:58:17 +0900

    go fmt

--- ch02/ex03/popcount_test.go
+++ ch02/ex03/popcount_test.go
@@ -7,15 +7,15 @@ import (
 func TestPopCountOfSeven(t *testing.T) {
 	actual := PopCount(7)
 	expected := 3
-    if actual != expected {
-        t.Errorf("got %v\nwant %v", actual, expected)
-    }
+	if actual != expected {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
 }
 
 func TestPopCountOfEight(t *testing.T) {
 	actual := PopCount(8)
 	expected := 1
-    if actual != expected {
-        t.Errorf("got %v\nwant %v", actual, expected)
-    }
+	if actual != expected {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
 }
