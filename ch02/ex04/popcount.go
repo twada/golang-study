fc53e0a43f180067018ba3a46ba38515085e4c4c
Takuto Wada <takuto.wada@gmail.com>
2018-03-16 09:16:15 +0900

    練習問題2.4 引数をビットシフトしながら最下位ビットの検査を64回繰り返すことでビット数を数える

---
+++ ch02/ex04/popcount.go
@@ -0,0 +1,12 @@
+package popcount
+
+func PopCount(x uint64) int {
+	var acc int
+	var current uint64
+	var i uint
+	for i = 0; i < 64; i++ {
+		current = x>>i
+		acc += int(current&1)
+	}
+	return acc
+}
