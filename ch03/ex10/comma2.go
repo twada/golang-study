ae14c92f624bd8c3079524219f1b795a0661c034
Takuto Wada <takuto.wada@gmail.com>
2018-04-13 11:34:26 +0900

    slice of runes, not bytes

--- ch03/ex10/comma2.go
+++ ch03/ex10/comma2.go
@@ -12,12 +12,13 @@ func comma(s string) string {
 			buf.WriteString(",")
 		}
 	}
-	return string(reverse(buf.Bytes()))
+	return reversed(buf.String())
 }
 
-func reverse(r []byte) []byte {
+func reversed(s string) string {
+	r := []rune(s)
 	for i, j := 0, len(r)-1; i < len(r)/2; i, j = i+1, j-1 {
 		r[i], r[j] = r[j], r[i]
 	}
-	return r
+	return string(r)
 }
