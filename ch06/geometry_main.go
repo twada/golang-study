bee03886de5fbb4477dd0d19288f38af51292401
Takuto Wada <takuto.wada@gmail.com>
2018-07-19 10:35:46 +0900

    block

--- ch06/geometry_main.go
+++ ch06/geometry_main.go
@@ -6,31 +6,36 @@ import (
 )
 
 func main () {
-	p := geometry.Point{1,2}
-	q := geometry.Point{4,6}
-	fmt.Println(geometry.Distance(p, q))
-	fmt.Println(p.Distance(q))
-
-	perim := geometry.Path{
-		{1, 1},
-		{5, 1},
-		{5, 4},
-		{1, 1},
+	{
+		p := geometry.Point{1,2}
+		q := geometry.Point{4,6}
+		fmt.Println(geometry.Distance(p, q))
+		fmt.Println(p.Distance(q))
 	}
-	fmt.Println(perim.Distance())
+
+	{
+		perim := geometry.Path{
+			{1, 1},
+			{5, 1},
+			{5, 4},
+			{1, 1},
+		}
+		fmt.Println(perim.Distance())
+	}
+
+	{
+		p := geometry.Point{1,2}
+		r := &geometry.Point{1, 2}
+		r.ScaleBy(2)
+		fmt.Println(*r)
+
+		pptr := &p
+		pptr.ScaleBy(2)
+		fmt.Println(p)
+
+		(&p).ScaleBy(2)
+		fmt.Println(p)
+	}
 
 
-	r := &geometry.Point{1, 2}
-	r.ScaleBy(2)
-	fmt.Println(*r)
-
-	pptr := &p
-	pptr.ScaleBy(2)
-	fmt.Println(p)
-
-	(&p).ScaleBy(2)
-	fmt.Println(p)
-
-
-	geometry.Point{1,2}.ScaleBy(2)
 }
