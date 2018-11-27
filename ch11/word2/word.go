9269fca82876d2510e3ae6469fd794fe48d2865e
Takuto Wada <takuto.wada@gmail.com>
2018-11-27 15:47:53 +0900

    バイト列ではなくルーン列を扱うように修正、非文字や大文字小文字の違いを無視
    回帰テストの内容に基づいて不具合修正

--- ch11/word2/word.go
+++ ch11/word2/word.go
@@ -1,8 +1,17 @@
 package word
 
+import "unicode"
+
 func IsPalindrome(s string) bool {
-	for i := range s {
-		if s[i] != s[len(s) - 1 - i] {
+	var letters []rune
+	for _, r := range s {
+		if unicode.IsLetter(r) {
+			letters = append(letters, unicode.ToLower(r))
+		}
+
+	}
+	for i := range letters {
+		if letters[i] != letters[len(letters) - 1 - i] {
 			return false
 		}
 	}
