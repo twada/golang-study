8fbe9993723539e44f4b2d5daf05753db01c7120
Takuto Wada <takuto.wada@gmail.com>
2018-11-29 12:09:18 +0900

    p.356 randomPalindrome

---
+++ ch11/randomPalindrome/randomPalindrome_test.go
@@ -0,0 +1,22 @@
+package randp
+
+import (
+	"github.com/twada/golang-study/ch11/word2"
+	"math/rand"
+	"time"
+	"testing"
+)
+
+func TestRandomPalindromes(t *testing.T) {
+	// 疑似乱数生成器を初期化する
+	seed := time.Now().UTC().UnixNano()
+	t.Logf("Random seed: %d", seed)
+	rng := rand.New(rand.NewSource(seed))
+
+	for i := 0; i < 1000; i++ {
+		p := randomPalindrome(rng)
+		if !word2.IsPalindrome(p) {
+			t.Errorf("IsPalindrome(%q) = false", p)
+		}
+	}
+}
--- ch11/word2/word_test.go
+++ ch11/word2/word_test.go
@@ -1,4 +1,4 @@
-package word
+package word2
 
 import (
 	"fmt"
