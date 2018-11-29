51d8d429cf0c5532ca04b0f964d9b898f87919ae
Takuto Wada <takuto.wada@gmail.com>
2018-11-30 00:57:49 +0900

    負の数しか扱わない

--- ch11/intmapset/intmapset.go
+++ ch11/intmapset/intmapset.go
@@ -12,11 +12,17 @@ func NewIntMapSet() *IntMapSet {
 
 // Has は負でない値 x をセットが含んでいるか否かを報告します
 func (s *IntMapSet) Has(x int) bool {
+	if x < 0 {
+		return false
+	}
 	_, ok := s.ints[x]
 	return ok
 }
 
 // Add セットに負ではない値 x を追加します
 func (s *IntMapSet) Add(x int) {
+	if x < 0 {
+		return
+	}
 	s.ints[x] = struct{}{}
 }
