386b8bf177df259aa4936f1e7078632cf72cd475
Takuto Wada <takuto.wada@gmail.com>
2018-11-29 14:19:46 +0900

    ex03 改善

--- ch11/ex03/randNonPalindromes_test.go
+++ ch11/ex03/randNonPalindromes_test.go
@@ -3,23 +3,11 @@ package ex03
 import (
 	"github.com/twada/golang-study/ch11/word2"
 	"math/rand"
+	"fmt"
 	"time"
 	"testing"
+	"unicode"
 )
-
-func randStr(rng *rand.Rand) string {
-	n := rng.Intn(20) + 4
-	runes := make([]rune, n)
-	runes = append(runes, rune('G'))
-	runes = append(runes, rune('o'))
-	for i := 2; i < n; i++ {
-		r := rune(rng.Intn(0x1000))
-		runes[i] = r
-	}
-	runes = append(runes, rune('G'))
-	runes = append(runes, rune('o'))
-	return string(runes)
-}
 
 func TestRandomNotPalindromes(t *testing.T) {
 	seed := time.Now().UTC().UnixNano()
@@ -27,8 +15,44 @@ func TestRandomNotPalindromes(t *testing.T) {
 	rng := rand.New(rand.NewSource(seed))
 	for i := 0; i < 1000; i++ {
 		p := randStr(rng)
-		if word2.IsPalindrome(p) {
-			t.Errorf("IsPalindrome(%q) = true", p)
-		}
+		t.Run(fmt.Sprintf("table#%d : %s", i, p), func(t *testing.T) {
+			if word2.IsPalindrome(p) {
+				t.Errorf("IsPalindrome(%q) = true", p)
+			}
+		})
 	}
+}
+
+func randStr(rng *rand.Rand) string {
+	alphas := upperAlphas()
+	n := rng.Intn(23) + 2
+	result := make([]rune, n)
+	for i := 0; i < n; i++ {
+		var r rune
+		r, alphas = randPick(alphas, rng)
+		if randBool(rng) {
+			r = unicode.ToLower(r)
+		}
+		result[i] = r
+	}
+	return string(result)
+}
+
+func randPick(from []rune, rng *rand.Rand) (rune, []rune) {
+	n := rng.Intn(len(from))
+	r := from[n]
+	to := append(from[0:n], from[n+1:]...)
+	return r, to
+}
+
+func randBool(rng *rand.Rand) bool {
+	return rng.Intn(1000) % 2 == 0
+}
+
+func upperAlphas() []rune {
+	runes := make([]rune, 0)
+	for i := 0x0041; i < 0x005b; i++ {
+		runes = append(runes, rune(i))
+	}
+	return runes;
 }
